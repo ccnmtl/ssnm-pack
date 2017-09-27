624a1a06c9c8e77e0e41f90375093d0fe8360fb3
Susan Dreher <sdreher@columbia.edu>
2017-09-27 09:09:35 -0400

    submit Person models as a separate pr

--- src/models.js
+++ src/models.js
@@ -5,63 +5,11 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var CryptoJS = require('crypto-js');
 
-// eslint-disable-next-line no-unused-vars
-var Proximity = {
-    VERY_CLOSE: 1,
-    SOMEWHAT_CLOSE: 2,
-    NOT_CLOSE: 3
-};
-
-//eslint-disable-next-line no-unused-vars
-var Influence = {
-    VERY_HELPFUL: 1,
-    SOMEWHAT_HELPFUL: 2,
-    NOT_HELPFUL: 3
-};
-
-//eslint-disable-next-line no-unused-vars
-var SupportType = {
-    EMPATHY: 1,
-    ADVICE: 2,
-    SOCIAL: 3,
-    PRACTICAL: 4
-};
-
-var Person = Backbone.Model.extend({
-    defaults: {
-        name: '',
-        proximity: undefined,
-        influence: undefined,
-        supportTypes: [],
-        notes: ''
-    },
-});
-
-var PersonList = Backbone.Collection.extend({
-    model: Person,
-    initialize: function(lst) {
-        if (lst !== undefined && lst instanceof Array) {
-            for (var i = 0; i < lst.length; i++) {
-                var x = new Person(lst[i]);
-                this.add(x);
-            }
-        }
-    },
-    toTemplate: function() {
-        var a = [];
-        this.forEach(function(item) {
-            a.push(item.toTemplate());
-        });
-        return a;
-    }
-});
-
 
 var SocialSupportMap = Backbone.Model.extend({
     defaults: {
         topic: '',
-        owner: '',
-        people: new PersonList()
+        owner: ''
     },
     toTemplate: function() {
         return _(this.attributes).clone();
@@ -69,7 +17,6 @@ var SocialSupportMap = Backbone.Model.extend({
     fromJSON: function(json) {
         this.set('topic', json.topic);
         this.set('owner', json.owner);
-        this.set('people', new PersonList(json.people));
     },
     encrypt: function(password) {
         var contents = JSON.stringify(this.toJSON());
