e270720ce18a0bda93b5ca2455fabe9e2fb320f5
Susan Dreher <sdreher@columbia.edu>
2017-11-02 17:13:45 -0400

    Position person containers

--- src/utils.js
+++ src/utils.js
@@ -9,4 +9,24 @@ function validateFormValue($form, selector) {
     $elt.addClass('is-invalid');
 }
 
+function eltCenter($elt) {
+    var offset = $elt.position();
+    return {
+        x: offset.left +  $elt.width() / 2,
+        y: offset.top +  $elt.height() / 2
+    };
+}
+
+function radius($elt) {
+    var shellHeight = $elt.prev().position().top - $elt.position().top;
+    return $elt.height() / 2 - shellHeight / 2;
+}
+
+function radians(angle) {
+    return angle * Math.PI / 180;
+}
+
 module.exports.validateFormValue = validateFormValue;
+module.exports.eltCenter = eltCenter;
+module.exports.radius = radius;
+module.exports.radians = radians;
