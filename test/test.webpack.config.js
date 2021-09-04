bae4905e25ed7d231dae05a4a677f8155494f34c
Susan Dreher <sdreher@gmail.com>
2021-09-04 09:44:01 -0400

    update syntax for webpack5 & update dependencies

--- test/test.webpack.config.js
+++ test/test.webpack.config.js
@@ -23,15 +23,11 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                type: 'asset/resource'
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
+                type: 'asset/resource'
             }
         ]
     }
